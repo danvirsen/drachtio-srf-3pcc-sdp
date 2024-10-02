e85e90df88880d5e3b73f29f6b93fa2139d9d3c8
Dan Virsén <dan.virsen@nwise.se>
2024-10-02 21:08:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+```shell
+# Run the test suite
+$ docker compose up --abort-on-container-exit
+
+# Check the logs for the Drachtio application
+$ docker compose logs drachtio-srf
+```
---
+++ app.js
@@ -0,0 +1,37 @@
+var Srf = require('drachtio-srf');
+var app = new Srf();
+
+app.invite(async (req, res) => {
+  try {
+    const { uas, uac } = await app.createB2BUA(req, res, "sip:service@172.29.0.11:6060");
+
+    console.log("Successfully set up call");
+
+    console.log(`\n=============== Call "${req.get("Subject")}" Start ===============\n`);
+    console.log("UAC Remote SDP:");
+    console.log(uac.remote.sdp + "\n");
+    console.log("UAS Remote SDP:");
+    console.log(uas.remote.sdp );
+    console.log(`=============== Call "${req.get("Subject")}" End ===============\n`);
+
+    uac.on("destroy", () => uas.destroy());
+    uas.on("destroy", () => uac.destroy());
+  } catch (e) {
+    console.error("Failed to set up call: ", e);
+  }
+});
+
+app.connect({
+  // host: "127.0.0.1",
+  host: "172.29.0.10",
+  port: 9022,
+  secret: "cymru",
+});
+
+app.on("connect", (_, hostport) => {
+  console.log(`connected to drachtio listening for SIP on hostport ${hostport}`);
+});
+
+app.on("error", (err) => {
+  console.log(`Error connecting to drachtio server: ${err.message}`);
+});
